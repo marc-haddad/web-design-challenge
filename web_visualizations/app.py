e870f9123173c34775ae64e309aaa1f61da5028d
Marc H <marc.omar.haddad@gmail.com>
2019-12-07 17:18:05 -0500

    Completed index and plots

--- web_visualizations/app.py
+++ web_visualizations/app.py
@@ -17,5 +17,21 @@ def compare():
 def data():
     return render_template("data.html")
 
+@app.route("/temp")
+def temp():
+    return render_template("temp.html")
+
+@app.route("/cloud")
+def cloud():
+    return render_template("cloud.html")
+
+@app.route("/wind")
+def wind():
+    return render_template("wind.html")
+
+@app.route("/hum")
+def hum():
+    return render_template("hum.html")
+
 if __name__=="__main__":
     app.run(debug=True)
